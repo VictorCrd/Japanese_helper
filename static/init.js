3434a75f8f50e42e553123017054a99ae98e163e
BOT-Spacegoat <victorcardon.vc@gmail.com>
2021-07-25 16:11:23 +0200

    keruko

--- static/init.js
+++
@@ -1,8 +0,0 @@
-(function($){
-  $(function(){
-    $('.parallax').parallax();
-    $('.collapsible').collapsible();
-    $('.scrollspy').scrollSpy();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
