9d4bd4b96668c258e4894a3504de1b8443fc757a
BOT-Spacegoat <victorcardon.vc@gmail.com>
2021-07-28 11:09:37 +0200

    youtube fixe

--- static/img_speed_speech.js
+++ static/img_speed_speech.js
@@ -3,7 +3,7 @@ function set_img_src(speed_of_speach) {
     if (speed_of_speach < 0.25) {
         image.src = "../static/img/rabbit.jpg";
     } else if (speed_of_speach < 0.5) {
-        image.src = "../static/img/inu.png";
+        image.src = "../static/img/inu.jpg";
     } else {
         image.src = "../static/img/turtle.jpg";
     }
